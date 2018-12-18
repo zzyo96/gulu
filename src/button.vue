5c747ef580b3a5a2f5bcf625ead379494ed78d5c
zzyo <1977795940@qq.com>
2018-12-18 23:12:33 +0800

    验证 iconPosition为left或right中的一个

--- src/button.vue
+++ src/button.vue
@@ -10,7 +10,17 @@
 </template>
 <script>
     export default {
-        props: ['icon', 'iconPosition']
+        // props: ['icon', 'iconPosition']
+        props: {
+            icon:{},
+            iconPosition:{
+                type:String,
+                default:'left',
+                validator: function (value) {
+                    return !(value !== 'left' && value !== 'right');
+                }
+            }
+        }
     }
 </script>
 <style lang="less">
