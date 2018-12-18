fbb225fcb21e6d83c69cc2cbd6f8c31866623f07
zzyo <1977795940@qq.com>
2018-12-18 22:49:08 +0800

    添加icon和iconPosition 属性

--- src/button.vue
+++ src/button.vue
@@ -1,25 +1,57 @@
 <template>
-    <button class='g-button'>按钮</button>
+    <button class='g-button' :class="{[`icon-${iconPosition}`]:true}">
+        <svg v-if="icon" class="icon">
+            <use :xlink:href=`#i-${icon}`></use>
+        </svg>
+        <div class="content">
+            <slot></slot>
+        </div>
+    </button>
 </template>
 <script>
-export default {}
+    export default {
+        props: ['icon', 'iconPosition']
+    }
 </script>
 <style lang="less">
-    .g-button{
+    .g-button {
         font-size: var(--font-size);
         height: var(--button-height);
         padding: 0 1em;
         border-radius: var(--border-radius);
-        border:1px solid var(--border-color);
+        border: 1px solid var(--border-color);
         background: var(--button-bg);
-       &:hover{
-            border-color:var(--border-color-hover)
+        display: inline-flex;
+        justify-content: center;
+        align-items: center;
+        vertical-align: middle;
+        &:hover {
+            border-color: var(--border-color-hover)
         }
-       &:active{
-            background-color:var(--button-active-bg)
+
+        &:active {
+            background-color: var(--button-active-bg)
         }
-       &:focus{
+
+        &:focus {
             outline: none;
+        }
+        > .icon{
+            order: 1;
+            margin-right: .1em;
+        }
+        > .content{
+            order: 2;
+        }
+        &.icon-right{
+            > .icon{
+                order: 2;
+                margin-right: 0;
+                margin-left: .1em;
+            }
+            > .content{
+                order: 1;
+            }
         }
     }
 </style>
