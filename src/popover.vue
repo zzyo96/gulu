8bdacc191fb3e87acb8e8b0dde6c16a92987750e
zzyo <1977795940@qq.com>
2019-04-03 14:02:11 +0800

    创建popover组件

---
+++ src/popover.vue
@@ -0,0 +1,49 @@
+<template>
+    <div class="popover" @click.stop="xxx">
+        <div class="content-wrapper" v-if="visible" @click.stop>
+            <slot name="content"></slot>
+        </div>
+        <slot></slot>
+    </div>
+</template>
+
+<script>
+  export default {
+    name: "GuluPopover",
+    data () {
+      return {visible: false}
+    },
+    methods: {
+      xxx () {
+        this.visible = !this.visible
+        if (this.visible === true) {
+          this.$nextTick(() => {
+            let eventHandler = () => {
+              this.visible = false
+              console.log('document 隐藏 popover')
+              document.removeEventListener('click', eventHandler)
+            }
+            document.addEventListener('click', eventHandler)
+          })
+        }else{
+          console.log('vm 隐藏 popover')
+        }
+      }
+    }
+  }
+</script>
+
+<style scoped lang="scss">
+    .popover {
+        display: inline-block;
+        vertical-align: top;
+        position: relative;
+        .content-wrapper {
+            position: absolute;
+            bottom: 100%;
+            left: 0;
+            border: 1px solid red;
+            box-shadow: 0 0 3px rgba(0, 0, 0, 0.5);
+        }
+    }
+</style>
