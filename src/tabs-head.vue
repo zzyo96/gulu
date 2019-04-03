3df4773b13027966118b84172f53fd96727d7dc1
zzyo <1977795940@qq.com>
2019-04-03 13:50:44 +0800

    完善tabs的样式以及添加tab切换时的动画

--- src/tabs-head.vue
+++ src/tabs-head.vue
@@ -11,9 +11,11 @@
   export default {
     name: 'GuluTabsHead',
     inject: ['eventBus'],
-    created () {
+    mounted () {
       this.eventBus.$on('update:selected', (item, vm) => {
-        console.log(item)
+        let {width, height, top, left} = vm.$el.getBoundingClientRect()
+        this.$refs.line.style.width = `${width}px`
+        this.$refs.line.style.left = `${left}px`
       })
     }
   }
@@ -21,20 +23,25 @@
 <style scoped lang="scss">
     $tab-height: 40px;
     $blue: blue;
+    $border-color: #ddd;
     .tabs-head {
         display: flex;
         height: $tab-height;
         justify-content: flex-start;
-        border: 1px solid red;
         position: relative;
+        border-bottom: 1px solid $border-color;
         > .line {
             position: absolute;
             bottom: 0;
             border-bottom: 1px solid $blue;
-            width: 100px;
+            transition: all 350ms;
         }
         > .actions-wrapper {
             margin-left: auto;
+            display: flex;
+            align-items: center;
+            justify-content: center;
+            padding: 0 1em;
         }
     }
 </style>
